c769bd7eead850982c77de3b5bccc159c3e3a84d
Kim Daeyong <dev.kimdaeyong@gmail.com>
2019-11-26 22:44:28 +0900

    [INIT] 프로젝트 초기화 기본 스크립트, 코아, mysql, typeorm, eslint, eslint-airbnb-babse 패키지 포함

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+
+import logger from 'koa-logger';
+import json from 'koa-json';
+
+import 'reflect-metadata';
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', async (ctx, next) => {
+  ctx.body = { msg: 'Hello, world' };
+
+  await next();
+});
+
+app.use(json());
+app.use(logger());
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000, () => {
+  console.log('Koa started');
+});
